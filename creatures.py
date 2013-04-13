05b9386d36b9bbbcb1b294b5edf82b7f057b38cc
Chris Overly <cqoverly@gmail.com>
2013-04-13 12:33:28 -0700

    Added breeding and gene inheritence.

--- creatures.py
+++ creatures.py
@@ -24,7 +24,7 @@ class Predator(Widget):
         super(Predator, self).__init__(*args, **kwargs)
         self.shape_genes = kwargs.get('shape_genes', ('r', 'e'))
         self.color_genes = kwargs.get('color_genes', ('b', 'r'))
-        self.offspring_genes = kwargs.get('offspring_genes', (5, 3))
+        self.offspring_genes = kwargs.get('offspring_genes', (1, 2))
         self.gender = random.choice(('M', 'F'))
         self.color = self.get_color()
         self.shape = self.get_shape()
@@ -68,6 +68,15 @@ class Predator(Widget):
         self.canvas.clear()
         self.draw()
 
+    def update_attrs(self):
+        self.age += 1
+        self.move()
+        self.is_dead()
+
+    def is_dead(self):
+        if self.age > self.lifespan:
+            self.parent.remove_widget(self)
+
 
 class World(Widget):
     count = 0
@@ -75,24 +84,31 @@ class World(Widget):
     eve = ObjectProperty(None)
 
     def random_position(self):
-        return (random.randint(5, self.width-5),
-                random.randint(5, self.height-5))
+        return (random.randint(21, self.width-21),
+                random.randint(21, self.height-21))
 
     def start_world(self):
         print self.size
-        adam = Predator(pos=self.random_position(),
-                        id='adam')
-        adam.gender = 'M'
-        self.add_widget(adam)
-        eve = Predator(pos=self.random_position(),
-                       id='eve')
-        eve.gender = 'F'
-        self.add_widget(eve)
-        print adam, "\n", eve
-        print adam.pos
-        print eve.pos
+        for i in range(5):
+            adam = Predator(pos=self.random_position(),
+                            id='adam')
+            adam.gender = 'M'
+            self.add_widget(adam)
+            eve = Predator(pos=self.random_position(),
+                           id='eve')
+            eve.gender = 'F'
+            self.add_widget(eve)
+
 
     def update(self, dt):
+        """
+        World.update is the main looping function for the program.
+        This function check at each frame interval to see what events occur.
+        Primarily, it checks calls creature udate methods to determine 
+        creature life events, as well as checks for collisions between 
+        creatures, at which point interaction events occur, such as potential
+        matings and births.
+        """
         if self.count <= 600:
             if self.count % 10 == 0:
                 for c in self.children:
@@ -120,31 +136,53 @@ class World(Widget):
             if c.x < self.x or c.x + c.width > self.width:
                 c.velocity_x *= -1
             c.velocity = c.velocity_x, c.velocity_y
-            c.move()
+            #  Check females for collisions.
+            if c.gender == 'F':
+                others = [p for p in self.children if p != c]
+                for p in others:
+                    if c.collide_widget(p) and p.gender == 'M':
+                        self.mating(c, p)
+            c.update_attrs()
+
         self.count += 1
 
     def mating(self, creatureA, creatureB):
         genders = (creatureA.gender, creatureB.gender)
+        spawn = 0
+        pop_factor = 0
         #  Make sure it's a M/F pairing.
         if 'F' not in genders or 'M' not in genders:
             pass
         else:
-            #  Get female 
-            f = [c for c in (creatureA, creatureB) if c.gender == 'F'][0]
-            #  And male
-            m = [c for c in (creatureA, creatureB) if c.gender == 'M'][0]                
-            #  Choose which birth genes to use.
-            spawn = random.choice(f.offspring_genes)
-            for i in range(spawn):
-                #  gather genes for children.
-                colors = (random.choice(f.color_genes),
-                          random.choice(m.color_genes))
-                shape = (random.choice(f.shape_genes),
-                         random.choice(m.shape_genes))
-                offspring = (random.choice(f.offspring_genes),
-                             random.choice(m.offspring_genes))
-                pos = (random.randint(1, self.size))
-                new_creature = Predator(pos=self.random_position())
+            #  Random chance of successful mating
+            if len(self.children)/10 > 10:
+                pop_factor = 150
+            else:
+                pop_factor = 50
+            if random.randint(1, pop_factor) == (10):   
+                #  Get female 
+                f = [c for c in (creatureA, creatureB) if c.gender == 'F'][0]
+                #  And male
+                m = [c for c in (creatureA, creatureB) if c.gender == 'M'][0]                
+                #  Choose which birth genes to use.
+                spawn = random.choice(f.offspring_genes)
+                for i in range(spawn):
+                    #  gather genes for children.
+                    colors = (random.choice(f.color_genes),
+                              random.choice(m.color_genes))
+                    shapes = (random.choice(f.shape_genes),
+                             random.choice(m.shape_genes))
+                    offspring = (random.choice(f.offspring_genes),
+                                 random.choice(m.offspring_genes))
+                    new_creature = Predator(pos=self.random_position(),
+                                            shape_genes=shapes,
+                                            color_genes=colors,
+                                            offspring_genes=offspring)
+                    self.add_widget(new_creature)
+                    print new_creature
+                print """
+                MATING! Babies: {0} / Total Predators: {1}
+                """.format(spawn, len(self.children))
 
 
 class WorldApp(App):
